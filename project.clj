0ab365dc61fb69fe3b4c62e8fae83057ccc7d13a
Daniel E. Renfer <duck@kronkltd.net>
2011-09-22 19:06:35 -0400

    update dependencies

--- project.clj
+++ project.clj
@@ -1,13 +1,12 @@
-(defproject karras "0.7.0"
+(defproject karras "0.7.1-SNAPSHOT"
   :description "A clojure entity framework for MongoDB"
-  :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]
+  :dependencies [[org.clojure/clojure "1.3.0-RC0"]
+                 [org.clojure.contrib/def "1.3.0-alpha4"]
                  [org.mongodb/mongo-java-driver "2.1"]
-                 [inflections "0.4.4"]]
-  :dev-dependencies [[scriptjure "0.1.9"
-                      :exclusions [org.clojure/clojure
-                                   org.clojure/clojure-contrib]]
-                     [midje "1.2.0"]]
-  :aot [karras.core karras.collection karras.sugar karras.entity]
+                 [inflections "0.5.2"]]
+  :dev-dependencies [[scriptjure "0.1.23"]
+                     [net.kronkltd/midje "1.3-alpha2-SNAPSHOT"]]
+  :exclusions [org.clojure/contrib
+               org.clojure/clojure-contrib]
   :autodoc {:web-src-dir "http://github.com/wilkes/karras/blob/"
             :web-home "http://wilkes.github.com/karras"})
